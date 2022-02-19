4f841a20954f0aa8789bd2e1c685b7f4f2122d58
darren lefcoe <dlefcoe@gmail.com>
2022-02-19 19:18:25 +0000

    .

---
+++ maths_quiz.py
@@ -0,0 +1,50 @@
+
+
+# types of case
+
+
+'''
+thisIsCamelCase
+this_is_snake_case
+ThisIsPascal
+This_Is_Mixed
+
+
+'''
+
+# from tkinter import N
+
+
+print('\nwelcome to nates maths quiz...\n')
+
+a = 29
+b = 54
+c = 956
+# ask user question and wait for response
+x = input(f'what is {a}x{b} ?\n')
+x = int(x)
+
+# answer is 1566
+if x == a*b:
+    print('correct' )
+    x = input(f'what is {b}x{c} ?\n') # answer is 51624
+    x = int(x)
+else:
+    print('\n you are thick\n') 
+    x = input(f'\n so thicko ill ask again what is {a}*{b} \n')
+if x == a*b:
+    print ('correct')
+    x = input(f'what is {b}x{c} ?\n') # answer is 51624
+    x = int(x)
+else: 
+    print('\n  i give up with you, i bet you dont even know 324x3498\n')
+    players_age = input('\n so how old are you thicko \n')
+    players_age = int(players_age)
+if players_age>= 90: 
+    players_age= int
+    print(' F*ck thats old')
+elif players_age < 20:
+    x = input('wow u really aint that old. as a computer i am older. ahahahahhahahahahahahahahaah')
+    
+    
+
